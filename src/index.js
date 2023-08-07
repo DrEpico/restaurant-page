492e11a2ee337487937d59cf765443ba8531d9fb
ali <adadashipour@gmail.com>
2023-08-07 19:14:20 +0100

    Deleted unwanted css and commented out js lines that didn't function

--- src/index.js
+++ src/index.js
@@ -35,41 +35,26 @@ document.addEventListener("DOMContentLoaded", function() {
         </div>
     `;
 
-    // Get the 'div#content' element
     var contentDiv = document.querySelector("#content");
 
-    // Append the content to the 'div#content' element
     contentDiv.innerHTML = contentInitial;
-
-    // var home = document.getElementById("home");
-    // var recipe = document.getElementById("recipe");
-    // var about = document.getElementById("about");
 
     // Add the click event listener to the parent element (delegating the event)
     contentDiv.addEventListener("click", function(event) {
         // Check if the clicked element has the ID "home"
         if (event.target.id === "home") {
-            // recipe.classList.remove("active-header");
-            // about.classList.remove("active-header");
-            // home.classList.add("active-header");
             contentDiv.innerHTML = contentInitial;
         }
     });
 
     contentDiv.addEventListener("click", function(event) {
         if (event.target.id === "recipe") {
-            // home.classList.remove("active-header");
-            // about.classList.remove("active-header");
-            // recipe.classList.add("active-header");
             contentDiv.innerHTML = contentRecipe;
         }
     })
 
     contentDiv.addEventListener("click", function(event) {
         if (event.target.id === "about") {
-            // home.classList.remove("active-header");
-            // recipe.classList.remove("active-header");
-            // about.classList.add("active-header");
             contentDiv.innerHTML = contentAbout;
         }
     })
